b826eeafb8b4882e0feafdaa99c254d12887317b
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-02-14 14:02:28 +0100

    gains tests for fake_factset_financial_data

--- tests/testthat/test-fake_factset_financial_data.R
+++ tests/testthat/test-fake_factset_financial_data.R
@@ -1,3 +1,25 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("by default, has expected names", {
+  out <- fake_factset_financial_data()
+
+  names <- names(out)
+  expected_names <- c(
+    "fsym_id",
+    "isin",
+    "factset_entity_id",
+    "adj_price",
+    "adj_shares_outstanding",
+    "issue_type",
+    "one_adr_eq"
+  )
+
+  expect_identical(sort(names), sort(expected_names))
+
+})
+
+test_that("by default, has expected values", {
+  out <- fake_factset_financial_data()
+
+  for (name in names(out)) {
+    expect_equal(out[[!!name]], default_values[[!!name]])
+  }
 })
