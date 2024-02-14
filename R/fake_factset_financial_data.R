8e3b2447c0ef6e90167415ec9cc016dbf2162ae9
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-02-14 14:02:15 +0100

    gains function for faking financial_data

--- R/fake_factset_financial_data.R
+++ R/fake_factset_financial_data.R
@@ -1,3 +1,35 @@
-fake_factset_financial_data <- function() {
-
+#' Minimal fake `factset_financial_data` dataset that allow overwriting values
+#'
+#' @template fake_data
+#'
+#' @return A data.frame
+#' @export
+#'
+#' @examples
+#' fake_factset_financial_data()
+#'
+#' fake_factset_financial_data(isin = "US0123456789")
+fake_factset_financial_data <- function(fsym_id = NULL,
+                                        isin = NULL,
+                                        factset_entity_id = NULL,
+                                        adj_price = NULL,
+                                        adj_shares_outstanding = NULL,
+                                        issue_type = NULL,
+                                        one_adr_eq = NULL,
+                                        ...) {
+  #TODO: Add checks that the input arguments match expectations, and error if not
+  # * check that inputs are the correct type
+  # * check that inputs are the expected length and char/numeric combination (e.g. for ISINs)
+  tibble::tibble(
+    fsym_id = fsym_id %||% default_values[["fsym_id"]],
+    isin = isin %||% default_values[["isin"]],
+    factset_entity_id =
+      factset_entity_id %||% default_values[["factset_entity_id"]],
+    adj_price = adj_price %||% default_values[["adj_price"]],
+    adj_shares_outstanding =
+      adj_shares_outstanding %||% default_values[["adj_shares_outstanding"]],
+    issue_type = issue_type %||% default_values[["issue_type"]],
+    one_adr_eq = one_adr_eq %||% default_values[["one_adr_eq"]],
+    ...
+  )
 }
---
+++ R/utils.R
@@ -0,0 +1,10 @@
+default_values <- list(
+  fsym_id = "A12345-B",
+  isin = "AB123CD4E567",
+  factset_entity_id = "1234A5-B",
+  adj_price = 12.34567,
+  adj_shares_outstanding = 1234567890,
+  issue_type = "EQ",
+  # one_adr_eq typically has values of 1, 0.25, 0.33, and other perfect fractions
+  one_adr_eq = 1/4
+)
