0b73c692953a79917478b1b119dc38ffa3ee96f5
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-02-15 10:37:17 +0100

    Alphabetize

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,11 @@
 default_values <- list(
-  fsym_id = "A12345-B",
-  isin = "AB123CD4E567",
-  factset_entity_id = "1234A5-B",
   adj_price = 12.34567,
   adj_shares_outstanding = 1234567890,
+  factset_entity_id = "1234A5-B",
+  fsym_id = "A12345-B",
+  isin = "AB123CD4E567",
   issue_type = "EQ",
-  # one_adr_eq typically has values of 1, 0.25, 0.33, and other perfect fractions
+  # Note: `one_adr_eq` typically has values of 1, 0.25, 0.33, and other fractions
   one_adr_eq = 1/4
 )
 
